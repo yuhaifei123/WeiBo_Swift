6349691d4014b84c9c9809fec5c5fc8a5db4635c
虞海飞 <1129639939@qq.com>
2016-10-08 13:17:11 +0800

    a

--- WeiBo_Swift/WeiBo_Swift/class/tool(工具)/ButtonTitle.swift
+++ WeiBo_Swift/WeiBo_Swift/class/tool(工具)/ButtonTitle.swift
@@ -10,14 +10,6 @@ import UIKit
 
 class ButtonTitle: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
     override init(frame: CGRect) {
         super.init(frame: frame);
 
