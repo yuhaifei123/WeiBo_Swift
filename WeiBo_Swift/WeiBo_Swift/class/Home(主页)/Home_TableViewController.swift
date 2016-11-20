4b32b0e650e52ca9e34545aa635148646febf641
yuhaifei <1129639939@qq.com>
2016-11-20 16:59:05 +0800

    qq

--- WeiBo_Swift/WeiBo_Swift/class/Home(主页)/Home_TableViewController.swift
+++ WeiBo_Swift/WeiBo_Swift/class/Home(主页)/Home_TableViewController.swift
@@ -32,16 +32,31 @@ class Home_TableViewController: All_TableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem.ItemBut(imageNameN: "navigationbar_pop", target: self, action: #selector(self.rightButClick));
 
         let titleButton = ButtonTitle();
-        titleButton.setTitle("aaaaaa ", for: UIControlState.normal);
+        titleButton.setTitle("微博 daemo ", for: UIControlState.normal);
                titleButton.addTarget(self, action: #selector(self.titeButton(btn:)), for: UIControlEvents.touchUpInside);
 
         navigationItem.titleView = titleButton;
     }
 
-
+    
+    /// 点击 button 后触发事件
+    ///
+    /// - Parameter btn: <#btn description#>
     @objc private func titeButton(btn : UIButton){
 
+        //按钮状态改变
         btn.isSelected = !btn.isSelected;
+        
+        let vc = UIStoryboard(name: "PopSB", bundle: nil);
+        let cn : UIViewController = vc.instantiateInitialViewController()!;
+        //设置转场动画代理
+        //如果，用默认的 modal，会把以前的控制器移除。
+        //有自定义，就不会，会有两个控制器
+        cn.transitioningDelegate = self;
+        //设置转场动画样式
+        cn.modalPresentationStyle = UIModalPresentationStyle.custom;
+        //转场方式
+        self.present(cn, animated: true, completion: nil);
     }
 
     func  leftBtnClick(){
@@ -49,8 +64,22 @@ class Home_TableViewController: All_TableViewController {
         print("aaaa");
     }
 
+    
     func rightButClick(){
 
         print("bbbbb");
     }
 }
+
+extension Home_TableViewController : UIViewControllerTransitioningDelegate{
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController?{
+        
+        return Pop_ViewController.init(presentedViewController: presented, presenting: presenting);
+    }
+    
+
+    
+    
+}
+
