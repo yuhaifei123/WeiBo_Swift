4f90590cbefb018c1e64cbd7f9272c802f813494
yuhaifei <1129639939@qq.com>
2016-12-20 16:25:29 +0800

    网格布局

--- WeiBo_Swift/WeiBo_Swift/AppDelegate.swift
+++ WeiBo_Swift/WeiBo_Swift/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window = UIWindow(frame: UIScreen.main.bounds);
         window?.backgroundColor = UIColor.white;
-        window?.rootViewController = Main_TabBarController();
+        window?.rootViewController = Newfeature_CollectionViewController(); //Main_TabBarController();
 
         window?.makeKeyAndVisible();
 
