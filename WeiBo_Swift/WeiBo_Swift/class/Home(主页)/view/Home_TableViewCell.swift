eb8cf33f74cdea9b4718ea69f0e3a792fd6f546a
yuhaifei <1129639939@qq.com>
2017-01-09 15:22:05 +0800

    九宫格，view

--- WeiBo_Swift/WeiBo_Swift/class/Home(主页)/view/Home_TableViewCell.swift
+++ WeiBo_Swift/WeiBo_Swift/class/Home(主页)/view/Home_TableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class Home_TableViewCell: UITableViewCell {
 
-    let XMGPictureViewCellReuseIdentifier = "XMGPictureViewCellReuseIdentifier";
+    var size : CGSize = CGSize(width: 0.001, height: 0.001);
     
     var status : Status? {
     
@@ -19,17 +19,11 @@ class Home_TableViewCell: UITableViewCell {
             //设置 topview 数据
             topView.status = status;
             contentLabel.text = status?.text;
-        
+            pictureView.status = status;
+            size = pictureView.calculateImageSize();
             pictureView.snp.updateConstraints { (make) in
-                make.size.equalTo(self.calculateImageSize());
-              //  make.bottom.equalTo(self.footerView.snp.top).offset(10);
+                make.size.equalTo(size);
             }
-            
-            footerView.snp.updateConstraints { (make) in
-                make.top.equalTo(pictureView.snp.bottom).offset(10);
-            }
-            // 1.3设置cell的大小
-            pictureLayout.itemSize = CGSize(width: 90, height: 90);
         }
     }
     
@@ -37,24 +31,7 @@ class Home_TableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier);
         
         setupUI();
-        setupPictureView();
-    }
-    
-    // 初始化配图 九宫格
-   func setupPictureView(){
-    
-        // 1.注册cell
-        pictureView.register(PictureViewCell.self, forCellWithReuseIdentifier: XMGPictureViewCellReuseIdentifier)
-        
-        // 2.设置数据源
-        pictureView.dataSource = self
-        
-        // 2.设置cell之间的间隙
-        pictureLayout.minimumInteritemSpacing = 10
-        pictureLayout.minimumLineSpacing = 10
-        
-        // 3.设置配图的背景颜色
-        pictureView.backgroundColor = UIColor.darkGray
+
     }
     
     /// 添加 view
@@ -89,7 +66,7 @@ class Home_TableViewCell: UITableViewCell {
             
             //make.top.equalTo(contentLabel.snp.bottom).offset(10);
             make.left.equalTo(10);
-            make.size.equalTo(CGSize(width: 1.0, height: 1.0));
+            make.size.equalTo(CGSize(width: 0.00001, height: 0.00001));
         }
         
         footerView.snp.makeConstraints { (make) in
@@ -117,56 +94,7 @@ class Home_TableViewCell: UITableViewCell {
         return footerView.frame.maxY
     }
     
-    /**
-     计算配图的尺寸
-     */
-    private func calculateImageSize() -> CGSize{
-    
-        //宽高
-        var size : CGSize?;
-        //固定宽高
-        let width = 90
-        let margin = 10
-        //那到所有的图片的总数
-        let count = status?.storedPicURLS?.count;
-        if count == nil || count == 0{
-            
-            size = CGSize(width: 0, height: 0);
-        }
-        else if (count == 1){
-            //等于1
-            //拿到路劲 key
-            let key = status?.storedPicURLS?[0].absoluteString;
-            //SDWebImageManager.shared().imageCache.imageFromMemoryCache(forKey: <#T##String!#>)
-            //拿到 image
-            let image = SDWebImageManager.shared().imageCache.imageFromMemoryCache(forKey: key);
-                
-                  size = image!.size;
-        }
-        else if (count == 4){
-            let viewWidth = width * 2 + margin * 2;
-            let viewHeight = width * 2 + margin;
-            
-            size = CGSize(width: viewWidth, height: viewHeight);
-        }
-        else{
-            
-            // 5.1计算列数
-            let colNumber = 3
-            // 5.2计算行数
-            //               (8 - 1) / 3 + 1
-            let rowNumber = (count! - 1) / 3 + 1
-            // 宽度 = 列数 * 图片的宽度 + (列数 - 1) * 间隙
-            let viewWidth = colNumber * width + (colNumber - 1) * margin
-            // 高度 = 行数 * 图片的高度 + (行数 - 1) * 间隙
-            let viewHeight = rowNumber * width + (rowNumber - 1) * margin
-            
-            size = CGSize(width: viewWidth, height: viewHeight);
-        }
-        
-        return size!;
-    }
-    
+       
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -195,80 +123,14 @@ class Home_TableViewCell: UITableViewCell {
         view.backgroundColor = UIColor(white: 0.2, alpha: 0.5);
         return view;
     }();
-    
-    /// 配图
-    private lazy var pictureLayout: UICollectionViewFlowLayout =  UICollectionViewFlowLayout()
+
     /// 配图  九宫格
-    private lazy var pictureView : UICollectionView = {
+    private lazy var pictureView : StatusPictureCollectionView = {
         
-        let view = UICollectionView(frame:CGRect.zero, collectionViewLayout: self.pictureLayout);
+        let view =  StatusPictureCollectionView();
         return view;
     }();
 }
 
-// MARK: - 九宫格图片
-extension Home_TableViewCell : UICollectionViewDataSource{
- 
-    /// 显示多少图片
-    ///
-    /// - Parameters:
-    ///   - collectionView: <#collectionView description#>
-    ///   - section: <#section description#>
-    /// - Returns: <#return value description#>
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return status?.storedPicURLS?.count ?? 0;
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // 1.取出cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: XMGPictureViewCellReuseIdentifier, for: indexPath) as! PictureViewCell
-        // 2.设置数据
-        //        cell.backgroundColor = UIColor.greenColor()
-        cell.imageURL = status?.storedPicURLS![indexPath.item]
-        // 3.返回cell
-        return cell
-    }
-}
 
 
-/// 设置九宫格的 cell
-class PictureViewCell : UICollectionViewCell{
-    
-    var imageURL : NSURL? {
-        
-        didSet{
-          
-           iconImageView.sd_setImage(with: imageURL as! URL)
-           // iconImageView(imageURL
-        }
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame);
-        
-        addView();
-    }
-    
-    /// 添加 view
-    private func addView(){
-        
-        contentView.addSubview(iconImageView);
-        iconImageView.snp.makeConstraints { (make) in
-            
-            make.top.left.bottom.right.equalTo(0);
-        }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-    private lazy var iconImageView : UIImageView = {
-    
-        let imageView = UIImageView();
-        return imageView;
-    }();
-}
